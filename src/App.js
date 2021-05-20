ce2afde02511a2c0c728a734e9a74b194dc88d92
monoharada <harada@monosus.co.jp>
2021-05-20 08:04:46 +0000

    learn useState

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 import Counter from './components/Counter';
 import CounterHook from './components/CounterHook';
+import FormHook from './components/FormHook';
+import ItemHook from './components/ItemHook';
 
 function App() {
   return (
     <div className="App">
+      <ItemHook />
+      <FormHook />
       <CounterHook />
       <Counter />
     </div>
--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 
 const CounterHook = () => {
-  const [count, setCount] = useState('')
+  const [count, setCount] = useState(0)
 
-  const incrementCount = () => {
-    setCount(count + 'o')
+  const incrementCount = () => setCount(prevCout => prevCout + 1)
+
+
+  const incrementCount10 = () => {
+    for (let index = 0; index < 10; index++) {
+      setCount(prevCout => prevCout + 1);
+
+    }
   }
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={incrementCount}>かうんと　＋</button>
+      <button onClick={incrementCount10}>かうんと　＋10</button>
     </div>
   )
 }
